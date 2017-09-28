ff0a4e31fb3bce475771e8b684df0f9178976a4e
Zechariah Robinson <zjrobinson90@gmail.com>
2017-09-28 16:13:34 -0700

    added a UI thing in buildbuildings

--- Assets/TriggerScript.cs
+++ Assets/TriggerScript.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class TriggerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Material[] material;
+    Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        rend.sharedMaterial = material[1];
+    }
+
+    void OnTriggerStay (Collider other)
+    {
+    }
+    void OnTriggerExit (Collider other)
+    {
+        rend.sharedMaterial = material[0];
+    }
+
 }
