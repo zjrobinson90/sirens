ac819dad0d6024b3757c4e5a322a3ab55474d546
Zechariah Robinson <zjrobinson90@gmail.com>
2017-10-19 21:08:56 -0700

    added a menu to hotdog world

---
+++ Assets/Scripts/TriggerSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSound : MonoBehaviour {
+
+	public AudioSource audio;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        audio.Play();
+    }
+	void OnTriggerStay (Collider other)
+    {
+		//Do nothing
+	}
+    void OnTriggerExit (Collider other)
+    {
+		//Do nothing
+	}
+}
